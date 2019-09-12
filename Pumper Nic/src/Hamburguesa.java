6c42f2adba2f7f4a9f2c37713e19854f782d3231
leozeballez <zeballezleonel@hotmail.com>
2019-09-12 17:02:40 -0300

    clase hamburguesa

---
+++ Pumper Nic/src/Hamburguesa.java
@@ -0,0 +1,12 @@
+public class Hamburguesa {
+    int medallon = 1;
+    int lechuga = 1;
+    int tomate = 1;
+
+    public void agregarMedallon() {
+        if(this.medallon == 4){
+        }else{
+            medallon += 1;
+        }
+    }
+}
