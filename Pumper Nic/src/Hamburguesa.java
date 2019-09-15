6068e595fc21caa0b67dd82a63cef9ae2f96212c
leozeballez <zeballezleonel@hotmail.com>
2019-09-15 20:03:43 -0300

    clase hamburguesa

--- Pumper Nic/src/Hamburguesa.java
+++ Pumper Nic/src/Hamburguesa.java
@@ -1,12 +1,83 @@
-public class Hamburguesa {
-    int medallon = 1;
-    int lechuga = 1;
-    int tomate = 1;
+public abstract class Hamburguesa {
+    private int medallon = 1;
+    public boolean lechuga = true;
+    public boolean tomate = true;
+    public boolean quesoAzul = false;
+    public boolean bacon = false;
+    public boolean huevo = false;
+    public boolean mollejas = false;
+    public boolean cebollasCaramelizadas = false;
+    public int precio;
 
     public void agregarMedallon() {
-        if(this.medallon == 4){
-        }else{
-            medallon += 1;
+        if(this.medallon != 4){
+            this.medallon += 1;
         }
+    }
+
+    public void agregarTomate() {
+        this.tomate = true;
+    }
+
+    public void agregarLechuga() {
+        this.lechuga = true;
+    }
+
+    public void agregarQuesoAzul() {
+        this.quesoAzul = true;
+    }
+
+    public void agregarBacon() {
+        this.bacon = true;
+    }
+
+    public void agregarHuevo() {
+        this.huevo = true;
+    }
+
+    public void agregarMollejas() {
+        this.mollejas = true;
+    }
+
+    public void agregarCebollasCaramelizadas() {
+        this.cebollasCaramelizadas = true;
+    }
+
+    public void quitarMedallon() {
+        if(this.medallon != 1) {
+            this.medallon -= 1;
+        }
+    }
+
+    public void quitarTomate() {
+        this.tomate = false;
+    }
+
+    public void quitarLechuga() {
+        this.lechuga = false;
+    }
+
+    public void quitarQuesoAzul() {
+        this.quesoAzul = false;
+    }
+
+    public void quitarBacon() {
+        this.bacon = false;
+    }
+
+    public void quitarHuevo() {
+        this.huevo = false;
+    }
+
+    public void quitarMollejas() {
+        this.mollejas = false;
+    }
+
+    public void quitarCebollasCaramelizadas() {
+        this.cebollasCaramelizadas = false;
+    }
+
+    public int costoFinal() {
+        return precio;
     }
 }
